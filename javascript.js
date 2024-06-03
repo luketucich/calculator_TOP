5e7a210ce4064070d0a9642b334b4826afe65aee
luketucich <ltucich@gmail.com>
2024-06-03 13:39:54 -0400

    Fixed a bug with the spacing between zeroes in the calculator, updated display text alignment, and created basic operator functions.

--- javascript.js
+++ javascript.js
@@ -1,21 +1,68 @@
+// Assigns buttons 
 const display = document.getElementById("display");
+const numberButtons = document.getElementsByClassName('button dark');
+const clearButton = document.getElementById("clear");
 
-const oneButton = document.getElementById("one");
-const twoButton = document.getElementById("two");
-const threeButton = document.getElementById("three");
-const fourButton = document.getElementById("four");
-const fiveButton = document.getElementById("five");
-const sixButton = document.getElementById("six");
-const sevenButton = document.getElementById("seven");
-const eightButton = document.getElementById("eight");
-const nineButton = document.getElementById("nine");
-const zeroButton = document.getElementById("zero");
+// Assigns calculator input and logic
+var firstNumber;
+var secondNumber;
+let operator;
+var buttonClickCount = 0
 
-let calculationList = []
+// Establishes operator functions
+function add(a, b) {
+    return a + b;
+}
 
+function subtract(a, b) {
+    return a - b;
+}
 
-nineButton.addEventListener("click", () => {
-    display.textContent = nineButton.textContent;
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// Handles what happens when the clear button is pressed
+clearButton.addEventListener('click', function () {
+    display.textContent = 0;
+    buttonClickCount = 0;
 });
+
+// Handles what happens when the numbers (0-9) and decimal key are pressed
+for (i of numberButtons) {
+    i.addEventListener('click', function () {
+        console.log(this);
+        buttonClickCount += 1;
+
+        // Keeps placeholder 0 if first key press was a decimal
+        if (buttonClickCount == 1 && this.textContent == ".") {
+            display.textContent += this.textContent;
+
+            // The first key press replaces placeholder zero
+        } else if (buttonClickCount == 1) {
+            display.textContent = this.textContent;
+
+            // Each subsequent key press is added onto the display
+        } else if (buttonClickCount > 1) {
+            display.textContent += this.textContent;
+
+        }
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
 
 
