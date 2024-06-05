b5393da9cd7363d28cb05c688252b48957e084d8
luketucich <ltucich@gmail.com>
2024-06-05 16:36:56 -0400

    Completed full calculatorfunctionality and UI

--- javascript.js
+++ javascript.js
@@ -1,13 +1,27 @@
-// Assigns buttons 
+// Display variables
 const display = document.getElementById("display");
+const displayContainer = document.getElementById("displayContainer");
+
+// Button variables
 const numberButtons = document.getElementsByClassName('button dark');
 const clearButton = document.getElementById("clear");
+const divideButton = document.getElementById("divide");
+const multiplyButton = document.getElementById("multiply");
+const subtractButton = document.getElementById("subtract");
+const addButton = document.getElementById("add");
+const equalButton = document.getElementById("equal");
+const operatorButtons = document.getElementsByClassName("orange");
+const posNegButton = document.getElementById("posNegContainer");
+const percentButton = document.getElementById("percent");
 
 // Assigns calculator input and logic
-var firstNumber;
-var secondNumber;
-let operator;
-var buttonClickCount = 0
+var firstNumber = 0;
+var secondNumber = 0;
+let operator = "none";
+var numberClickCount = 0
+let isSelected = false;
+
+// let addIsSelected = false
 
 // Establishes operator functions
 function add(a, b) {
@@ -28,41 +42,216 @@ function divide(a, b) {
 
 // Handles what happens when the clear button is pressed
 clearButton.addEventListener('click', function () {
+
+    // Resets display text and numbers
     display.textContent = 0;
-    buttonClickCount = 0;
+    firstNumber = 0;
+    secondNumber = 0;
+
+    // Deselects all operators
+    isSelected = false;
+    operator = "none";
+    for (i of operatorButtons) {
+        i.style.backgroundColor = "#ff9e0b";
+    }
 });
 
-// Handles what happens when the numbers (0-9) and decimal key are pressed
+// Numbers (0-9) or decimal key are pressed
 for (i of numberButtons) {
     i.addEventListener('click', function () {
-        console.log(this);
-        buttonClickCount += 1;
+        // When you press a number/decimal when add is selected
+        if (isSelected == true) {
+            isSelected = false;
+            for (i of operatorButtons) {
+                i.style.backgroundColor = "#ff9e0b";
+            }
+            display.textContent = this.textContent;
 
-        // Keeps placeholder 0 if first key press was a decimal
-        if (buttonClickCount == 1 && this.textContent == ".") {
+            // Does not allow user to add more than one decimal to a number
+        } else if (display.textContent.includes('.') && this.textContent == '.') {
+
+            // Keeps placeholder 0 if first key press was a decimal
+        } else if (display.textContent == "0" && this.textContent == ".") {
             display.textContent += this.textContent;
 
             // The first key press replaces placeholder zero
-        } else if (buttonClickCount == 1) {
+        } else if (display.textContent == "0") {
             display.textContent = this.textContent;
 
             // Each subsequent key press is added onto the display
-        } else if (buttonClickCount > 1) {
+        } else {
             display.textContent += this.textContent;
-
         }
     });
 };
 
+// Add button is pressed
+addButton.addEventListener('click', function () {
+    if (isSelected == false) {
+        operator = "add";
+        isSelected = true;
+        addButton.style.backgroundColor = "#ffd492";
+        firstNumber = Number(display.textContent);
 
+        // You select add when an operator is in use
+    } else if (isSelected == true && operator !== "add") {
+        // Makes current operator add
+        operator = "add";
+        addButton.style.backgroundColor = "#ffd492";
 
+        // Changes all other operator background colors
+        subtractButton.style.backgroundColor = "#ff9e0b";
+        multiplyButton.style.backgroundColor = "#ff9e0b";
+        divideButton.style.backgroundColor = "#ff9e0b";
 
+    } else if (isSelected == true && operator == "add") {
+        operator = "none";
+        isSelected = false;
+        addButton.style.backgroundColor = "#ff9e0b";
+    }
+});
 
+// Subtract button is pressed
+subtractButton.addEventListener('click', function () {
+    // You select subtract when no operators are in use
+    if (isSelected == false) {
+        operator = "subtract";
+        isSelected = true;
+        subtractButton.style.backgroundColor = "#ffd492";
+        firstNumber = Number(display.textContent);
 
+        // You select subtract when an operator is in use
+    } else if (isSelected == true && operator !== "subtract") {
+        // Makes current operator add
+        operator = "subtract";
+        subtractButton.style.backgroundColor = "#ffd492";
 
+        // Changes all other operator background colors
+        addButton.style.backgroundColor = "#ff9e0b";
+        multiplyButton.style.backgroundColor = "#ff9e0b";
+        divideButton.style.backgroundColor = "#ff9e0b";
 
+        // You deselect subtract
+    } else if (isSelected == true && operator == "subtract") {
+        operator = "none";
+        isSelected = false;
+        subtractButton.style.backgroundColor = "#ff9e0b";
+    }
+});
 
+// Multiply button is pressed
+multiplyButton.addEventListener('click', function () {
+    // You select subtract when no operators are in use
+    if (isSelected == false) {
+        operator = "multiply";
+        isSelected = true;
+        multiplyButton.style.backgroundColor = "#ffd492";
+        firstNumber = Number(display.textContent);
 
+        // You select subtract when an operator is in use
+    } else if (isSelected == true && operator !== "multiply") {
+        // Makes current operator add
+        operator = "multiply";
+        multiplyButton.style.backgroundColor = "#ffd492";
 
+        // Changes all other operator background colors
+        subtractButton.style.backgroundColor = "#ff9e0b";
+        addButton.style.backgroundColor = "#ff9e0b";
+        divideButton.style.backgroundColor = "#ff9e0b";
+
+        // You deselect subtract
+    } else if (isSelected == true && operator == "multiply") {
+        operator = "none";
+        isSelected = false;
+        multiplyButton.style.backgroundColor = "#ff9e0b";
+    }
+});
+
+// Divide button is pressed
+divideButton.addEventListener('click', function () {
+    // You select subtract when no operators are in use
+    if (isSelected == false) {
+        operator = "divide";
+        isSelected = true;
+        divideButton.style.backgroundColor = "#ffd492";
+        firstNumber = Number(display.textContent);
+
+        // You select subtract when an operator is in use
+    } else if (isSelected == true && operator !== "divide") {
+        // Makes current operator add
+        operator = "divide";
+        divideButton.style.backgroundColor = "#ffd492";
+
+        // Changes all other operator background colors
+        subtractButton.style.backgroundColor = "#ff9e0b";
+        addButton.style.backgroundColor = "#ff9e0b";
+        multiplyButton.style.backgroundColor = "#ff9e0b";
+
+        // You deselect subtract
+    } else if (isSelected == true && operator == "divide") {
+        operator = "none";
+        isSelected = false;
+        divideButton.style.backgroundColor = "#ff9e0b";
+    }
+});
+
+posNegButton.addEventListener('click', function () {
+    if (display.textContent.includes("-")) {
+        var newNumber = Number(display.textContent.replace("-", ""));
+        display.textContent = newNumber
+
+    } else {
+        var newNumber = Number(`-${display.textContent}`);
+        display.textContent = newNumber;
+    }
+})
+
+percentButton.addEventListener('click', function () {
+    var newNumber = (Number(display.textContent)) / 100;
+    display.textContent = newNumber
+})
+
+// Equal button is pressed
+equalButton.addEventListener('click', function () {
+    secondNumber = Number(display.textContent);
+
+    if (operator == "add") {
+        // Updates display content and numbers
+        display.textContent = add(firstNumber, secondNumber).toFixed(3);
+        firstNumber = Number(display.textContent);
+        secondNumber = 0;
+
+        // Deselects operators
+        operator = "none";
+        isSelected = false;
+    } else if (operator == "subtract") {
+        // Updates display content and numbers
+        display.textContent = subtract(firstNumber, secondNumber).toFixed(3);
+        firstNumber = Number(display.textContent);
+        secondNumber = 0;
+
+        // Deselects operators
+        operator = "none";
+        isSelected = false;
+    } else if (operator == "multiply") {
+        // Updates display content and numbers
+        display.textContent = multiply(firstNumber, secondNumber).toFixed(3);
+        firstNumber = Number(display.textContent);
+        secondNumber = 0;
+
+        // Deselects operators
+        operator = "none";
+        isSelected = false;
+    } else if (operator == "divide") {
+        // Updates display content and numbers
+        display.textContent = divide(firstNumber, secondNumber).toFixed(3);
+        firstNumber = Number(display.textContent);
+        secondNumber = 0;
+
+        // Deselects operators
+        operator = "none";
+        isSelected = false;
+    }
+});
 
 
